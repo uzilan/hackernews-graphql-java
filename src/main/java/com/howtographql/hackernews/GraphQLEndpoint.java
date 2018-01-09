620bf3975ad2e3928f060d8fec7310e47fc98fe8
Uzi Landsmann <uzi.landsmann@gmail.com>
2018-01-09 15:02:08 +0100

    completed connectors

--- src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
+++ src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
@@ -4,21 +4,30 @@ import com.coxautodev.graphql.tools.SchemaParser;
 import com.howtographql.hackernews.opreations.Mutation;
 import com.howtographql.hackernews.opreations.Query;
 import com.howtographql.hackernews.repositories.LinkRepository;
+import com.mongodb.MongoClient;
 import graphql.schema.GraphQLSchema;
 import graphql.servlet.SimpleGraphQLServlet;
+import lombok.val;
 
 import javax.servlet.annotation.WebServlet;
 
 @WebServlet(urlPatterns = "/graphql")
 public class GraphQLEndpoint extends SimpleGraphQLServlet {
 
+    private static final LinkRepository linkRepository;
+
+    static {
+        //Change to `new MongoClient("mongodb://<host>:<port>/hackernews")`
+        //if you don't have Mongo running locally on port 27017
+        val mongo = new MongoClient().getDatabase("hackernews");
+        linkRepository = new LinkRepository(mongo.getCollection("links"));
+    }
+
     public GraphQLEndpoint() {
         super(buildSchema());
     }
 
     private static GraphQLSchema buildSchema() {
-        LinkRepository linkRepository = new LinkRepository();
-
         return SchemaParser.newParser()
                 .file("schema.graphqls")
                 .resolvers(new Query(linkRepository), new Mutation(linkRepository))
--- src/main/java/com/howtographql/hackernews/models/Link.java
+++ src/main/java/com/howtographql/hackernews/models/Link.java
@@ -1,9 +1,17 @@
 package com.howtographql.hackernews.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Link {
+    private final String id;
     private final String url;
     private final String description;
+
+
+    public Link(String url, String description) {
+        this(null, url, description);
+    }
 }
