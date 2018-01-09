1718ed3d046208ebddc9a41ca619062964cbd11b
Uzi Landsmann <uzi.landsmann@gmail.com>
2018-01-09 14:54:10 +0100

    completed mutations

--- src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
+++ src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
@@ -1,6 +1,7 @@
 package com.howtographql.hackernews;
 
 import com.coxautodev.graphql.tools.SchemaParser;
+import com.howtographql.hackernews.opreations.Mutation;
 import com.howtographql.hackernews.opreations.Query;
 import com.howtographql.hackernews.repositories.LinkRepository;
 import graphql.schema.GraphQLSchema;
@@ -20,7 +21,7 @@ public class GraphQLEndpoint extends SimpleGraphQLServlet {
 
         return SchemaParser.newParser()
                 .file("schema.graphqls")
-                .resolvers(new Query(linkRepository))
+                .resolvers(new Query(linkRepository), new Mutation(linkRepository))
                 .build()
                 .makeExecutableSchema();
     }
